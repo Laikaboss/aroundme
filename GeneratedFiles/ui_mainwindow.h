795a8dd3ebf00f9c5eb6418079b15129e53ba675
Yaroslav92 <yaroslav.radushkin@mail.ru>
2014-11-10 00:33:41 +0300

    Херня не работает

--- GeneratedFiles/ui_mainwindow.h
+++ GeneratedFiles/ui_mainwindow.h
@@ -17,8 +17,6 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
-#include <QtWidgets/QStatusBar>
-#include <QtWidgets/QToolBar>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
@@ -28,55 +26,117 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralWidget;
-    QWidget *verticalLayoutWidget;
+    QWidget *verticalWidget;
     QVBoxLayout *verticalLayout;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_2;
     QPushButton *pushButton;
     QMenuBar *menuBar;
-    QToolBar *mainToolBar;
-    QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(818, 665);
+        MainWindow->resize(542, 521);
         QSizePolicy sizePolicy(QSizePolicy::Maximum, QSizePolicy::Expanding);
         sizePolicy.setHorizontalStretch(0);
         sizePolicy.setVerticalStretch(100);
         sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
         MainWindow->setSizePolicy(sizePolicy);
+        MainWindow->setStyleSheet(QLatin1String("QMainWindow \n"
+"{\n"
+"	 background: rgb(150, 150, 150);\n"
+"}\n"
+"\n"
+"\n"
+""));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        verticalLayoutWidget = new QWidget(centralWidget);
-        verticalLayoutWidget->setObjectName(QStringLiteral("verticalLayoutWidget"));
-        verticalLayoutWidget->setGeometry(QRect(0, 0, 201, 611));
-        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalWidget = new QWidget(centralWidget);
+        verticalWidget->setObjectName(QStringLiteral("verticalWidget"));
+        verticalWidget->setGeometry(QRect(0, 0, 251, 651));
+        QSizePolicy sizePolicy1(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(verticalWidget->sizePolicy().hasHeightForWidth());
+        verticalWidget->setSizePolicy(sizePolicy1);
+        verticalWidget->setStyleSheet(QLatin1String("QWidget\n"
+"{\n"
+"	background-color:rgb(85,85,85);\n"
+"	width:10px;\n"
+"}\n"
+"QPushButton \n"
+"{\n"
+"     background: rgb(85, 85, 85); \n"
+"	 border-style: outset;\n"
+"     border-width: 1px;\n"
+"     border-color:rgb(50, 50, 50);\n"
+"	 color:rgb(240, 240, 240);\n"
+"	 min-height:45%;\n"
+"	width:10px;\n"
+"	text-shadow: 2px 4px 3px rgba(0,0,0,0.3);\n"
+"\n"
+"}\n"
+"QPushButton:hover {\n"
+"     background-color:  rgb(105, 105, 105);\n"
+" }\n"
+"\n"
+""));
+        verticalLayout = new QVBoxLayout(verticalWidget);
         verticalLayout->setSpacing(0);
         verticalLayout->setContentsMargins(11, 11, 11, 11);
         verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
         verticalLayout->setSizeConstraint(QLayout::SetDefaultConstraint);
         verticalLayout->setContentsMargins(0, 0, 0, 0);
-        pushButton = new QPushButton(verticalLayoutWidget);
-        pushButton->setObjectName(QStringLiteral("pushButton"));
-        QSizePolicy sizePolicy1(QSizePolicy::Minimum, QSizePolicy::Maximum);
-        sizePolicy1.setHorizontalStretch(0);
-        sizePolicy1.setVerticalStretch(0);
-        sizePolicy1.setHeightForWidth(pushButton->sizePolicy().hasHeightForWidth());
-        pushButton->setSizePolicy(sizePolicy1);
+        pushButton_3 = new QPushButton(verticalWidget);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+        QSizePolicy sizePolicy2(QSizePolicy::Minimum, QSizePolicy::Maximum);
+        sizePolicy2.setHorizontalStretch(0);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(pushButton_3->sizePolicy().hasHeightForWidth());
+        pushButton_3->setSizePolicy(sizePolicy2);
+        pushButton_3->setMinimumSize(QSize(0, 47));
+        pushButton_3->setBaseSize(QSize(0, 0));
+        QFont font;
+        font.setFamily(QStringLiteral("Domkrat"));
+        font.setPointSize(12);
+        font.setBold(true);
+        font.setItalic(true);
+        font.setWeight(75);
+        pushButton_3->setFont(font);
 
-        verticalLayout->addWidget(pushButton);
+        verticalLayout->addWidget(pushButton_3);
+
+        pushButton_2 = new QPushButton(verticalWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        sizePolicy2.setHeightForWidth(pushButton_2->sizePolicy().hasHeightForWidth());
+        pushButton_2->setSizePolicy(sizePolicy2);
+        pushButton_2->setBaseSize(QSize(0, 0));
+        QFont font1;
+        font1.setFamily(QStringLiteral("Domkrat"));
+        font1.setPointSize(12);
+        font1.setBold(true);
+        font1.setItalic(false);
+        font1.setWeight(75);
+        font1.setStyleStrategy(QFont::PreferAntialias);
+        pushButton_2->setFont(font1);
+
+        verticalLayout->addWidget(pushButton_2);
+
+        pushButton = new QPushButton(verticalWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        sizePolicy2.setHeightForWidth(pushButton->sizePolicy().hasHeightForWidth());
+        pushButton->setSizePolicy(sizePolicy2);
+        pushButton->setBaseSize(QSize(0, 0));
+        pushButton->setFont(font1);
+
+        verticalLayout->addWidget(pushButton, 0, Qt::AlignTop);
 
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 818, 21));
+        menuBar->setGeometry(QRect(0, 0, 542, 21));
         MainWindow->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(MainWindow);
-        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
-        statusBar = new QStatusBar(MainWindow);
-        statusBar->setObjectName(QStringLiteral("statusBar"));
-        MainWindow->setStatusBar(statusBar);
 
         retranslateUi(MainWindow);
 
@@ -86,7 +146,9 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
-        pushButton->setText(QApplication::translate("MainWindow", "PushButton", 0));
+        pushButton_3->setText(QApplication::translate("MainWindow", "text", 0));
+        pushButton_2->setText(QApplication::translate("MainWindow", "text", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "text", 0));
     } // retranslateUi
 
 };
--- main.cpp
+++ main.cpp
@@ -2,31 +2,23 @@
 #include <QApplication>
 #include <QtGui>
 #include <QPushButton>
+#include <qlayout.h>
+
 int main(int argc, char *argv[])
 {
+
 	QApplication a(argc, argv);
 	MainWindow w;
-	QPushButton *btn = new QPushButton(&w);
-	qApp->setStyleSheet(
-		"QPushButton {"
-		"border: 1px solid #000000;"
-		"border-radius: 10px;"
-		"background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,"
-		"stop: 0 #dedede, stop: 0.5 #434343, stop: 0.51 #000000, stop: 1 #656a6d);"
-		"min-width: 80px;"
-		"}"
+	QWidget* wd = w.findChild<QWidget*>("verticalWidget");
 
-		"QPushButton:pressed {"
-		"background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,"
-		"stop: 0 #cfcccc, stop: 0.5 #333232, stop: 0.51 #000000, stop: 1 #585c5f);"
-		"}"
-
-		"QPushButton:flat {"
-		"border: none; "
-		"}"
-		);
-
-	btn->move(160, 150);
 	w.show();
 	return a.exec();
+}
+
+void QWidget::enterEvent(QEvent * event)
+{
+	MainWindow w;
+	QWidget* wd = w.findChild<QWidget*>("verticalWidget");
+	wd->move(100, 1000);
+	this->repaint();
 }
